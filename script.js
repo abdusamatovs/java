35da4c39f5e54d6ae03868ebc24dcd7fda76a76d
abdusamatovs <147515892+abdusamatovs@users.noreply.github.com>
2023-12-22 21:19:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstNum = prompt("1 son kiriting");
+let secondNum = prompt("2 son kiriting");
+
+firstNum++;
+
+secondNum--;
+
+console.log(
+  `Siz kiritgan 1-son ${firstNum} ga o'zgardi\nSiz kiritgan 2-son ${secondNum} ga o'zgartirildi `
+);
